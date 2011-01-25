241ca2e8cdaa3815874f0df6e847ee4372546bbf
popoca <el_chav0_del_8@hotmail.com>
2011-01-25 17:30:04 -0600

    Pues no funciona, supongo porque el stopwatch de la camara no es el mismo que de la winform, no es como el gamtime, or idk XD

--- Camera.cs
+++ Camera.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
+using Microsoft.Xna;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
@@ -20,10 +22,28 @@ namespace Stereo
         public Matrix view { get; protected set; }
         public Matrix projection { get; protected set; }
 
+        // Camera vectors
+        public Vector3 cameraPosition { get; protected set; }
+        Vector3 cameraDirection;
+        Vector3 cameraUp;
+
+        // Speed
+        float speed = 3;
+
+        // Mouse stuff
+        MouseState prevMouseState;
+
         public Camera( GraphicsDevice graphicsDevice, Vector3 pos, Vector3 target, Vector3 up)
         {
-            // Initialize view matrix
-            view = Matrix.CreateLookAt(pos, target, up);
+            /*// Initialize view matrix
+            view = Matrix.CreateLookAt(pos, target, up);*/
+
+            // Build camera view matrix
+            cameraPosition = pos;
+            cameraDirection = target - pos;
+            cameraDirection.Normalize();
+            cameraUp = up;
+            CreateLookAt();
 
             // Initialize projection matrix
             projection = Matrix.CreatePerspectiveFieldOfView(
@@ -41,7 +61,10 @@ namespace Stereo
         /// </summary>
         public override void Initialize()
         {
-            // TODO: Add your initialization code here
+            // Set mouse position and do initial get state
+            Mouse.SetPosition((float)graphicsDevice.Viewport.Width / 2,
+            (float)graphicsDevice.Viewport.Height / 2);
+            prevMouseState = Mouse.GetState();
 
             base.Initialize();
         }
@@ -50,11 +73,56 @@ namespace Stereo
         /// Allows the game component to update itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        public override void Update(GameTime gameTime)
+        public override void Update(Stopwatch stopwatch)
         {
-            // TODO: Add your update code here
+            // Move forward/backward
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+                cameraPosition += cameraDirection * speed;
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+                cameraPosition -= cameraDirection * speed;
+            // Move side to side
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+                cameraPosition += Vector3.Cross(cameraUp, cameraDirection) * speed;
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+                cameraPosition -= Vector3.Cross(cameraUp, cameraDirection) * speed;
 
-            base.Update(gameTime);
+            // Yaw rotation
+            cameraDirection = Vector3.Transform(cameraDirection,
+                Matrix.CreateFromAxisAngle(cameraUp, (-MathHelper.PiOver4 / 150) *
+                (Mouse.GetState().X - prevMouseState.X)));
+
+            // Roll rotation
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                cameraUp = Vector3.Transform(cameraUp,
+                    Matrix.CreateFromAxisAngle(cameraDirection,
+                    MathHelper.PiOver4 / 45));
+            }
+            if (Mouse.GetState().RightButton == ButtonState.Pressed)
+            {
+                cameraUp = Vector3.Transform(cameraUp,
+                    Matrix.CreateFromAxisAngle(cameraDirection,
+                    -MathHelper.PiOver4 / 45));
+            }
+
+            // Pitch rotation
+            cameraDirection = Vector3.Transform(cameraDirection,
+                Matrix.CreateFromAxisAngle(Vector3.Cross(cameraUp, cameraDirection),
+                (MathHelper.PiOver4 / 100) *
+                (Mouse.GetState().Y - prevMouseState.Y)));
+
+            cameraUp = Vector3.Transform(cameraUp,
+                Matrix.CreateFromAxisAngle(Vector3.Cross(cameraUp, cameraDirection),
+                (MathHelper.PiOver4 / 100) *
+                (Mouse.GetState().Y - prevMouseState.Y)));
+
+            // Reset prevMouseState
+            prevMouseState = Mouse.GetState();
+
+            // Recreate the camera view matrix
+            CreateLookAt();
+
+            base.Update(stopwatch);
         }
     }
 }
