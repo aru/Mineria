a3ca62131e2950087cba8ed7f66d93a3adce4baa
Aru <albinkun@gmail.com>
2011-02-03 16:26:27 -0600

    lulz

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace Stereo
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
